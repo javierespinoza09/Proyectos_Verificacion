842b5559e02126b859b352aa49da796c0f5f9ca7
javierespinoza <javierespriv44@gmail.com>
2023-09-18 23:49:58 -0600

    Manejador de colas probado

--- Proyecto_1/development/queue_handler.sv
+++ Proyecto_1/development/queue_handler.sv
@@ -8,13 +8,19 @@ class queue_handler;
 
     task print_q1();
         foreach (q1[i]) begin
-            $display("De la cola q1 en la pos %0d el valor es %0d",i,q1[i])
+            $display("De la cola q1 en la pos %0d el valor es %0d",i,q1[i]);
         end
     endtask
 
     task print_q2();
         foreach (q2[i]) begin
-            $display("De la cola q1 en la pos %0d el valor es %0d",i,q2[i])
+            $display("De la cola q1 en la pos %0d el valor es %0d",i,q2[i]);
         end
     endtask
+
+    function int pndng_q1();
+	    int p;
+	    if (q1.size==0) return 0;
+	    else return 1;
+    endfunction
 endclass
--- Proyecto_1/development/queue_tb.sv
+++ Proyecto_1/development/queue_tb.sv
@@ -1,10 +1,17 @@
 `include "queue_handler.sv"
 module queue_tb();
+    int p;
+    initial begin
     queue_handler Q0;
-    Q0 = new();
-    for (int i = 0, i>10, i++) begin
+    Q0 = new(); 
+    p = Q0.pndng_q1();
+    $display("Pending = %0d",p);
+    for (int i = 0; i<10; i++) begin
         Q0.q1.push_back(i);
     end
+    p = Q0.pndng_q1();
+    $display("Pending = %0d",p);
     Q0.print_q1;
+    end
 endmodule
 
