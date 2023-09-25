57da4432d2c26dffcabfeda5d9e9f8dd075c45e3
javierespinoza09 <javierespriv44@gmail.com>
2023-09-24 21:39:35 -0600

    Prueba driver

--- Proyecto_1/development/example_driver.sv
+++ Proyecto_1/development/example_driver.sv
@@ -4,7 +4,7 @@ class example_driver;
 	virtual bus_if #(.drvrs(drvrs), .pckg_sz(pckg_sz)) v_if;
 	int automatic drv_num;
 	rand bit [pckg_sz-9:0] payload;
-	rand bit [8:0] id;
+	rand bit [7:0] id;
 
 	constraint valid_addrs {id < $clog2(drvrs)};
 
@@ -14,15 +14,21 @@ class example_driver;
 	endfunction	
 
 	task run();
-		v_if.pndng[this.drv_num] = 1;
-		v_if.D_pop[this.drv_num] = {this.id,this.payload};
-		if (v_if.pop[this.drv_num]) begin
-			#1
-			v_if.pndng[this.drv_num] = 0;
+		v_if.pndng[0][this.drv_num] = 1;
+		while (v_if.pndng[0][this.drv_num]) begin	
+			v_if.D_pop[0][this.drv_num] = {this.id,this.payload};
+			if (v_if.pop[0][this.drv_num]) begin
+				#1
+				v_if.pndng[0][this.drv_num] = 0;
+			end
+			if (v_if.push[0][this.drv_num]) begin
+				$display("Device_%b Dato Recibido %b",this.drv_num,v_if.D_push[this.drv_num])
+			end
+			
 		end
-		if (v_if.push[this.drv_num]) begin
-			$display("Device_%d Dato Recibido %b",this.drv_num,v_if.D_push[this.drv_num])
 	endtask
-
+	function display();
+		$display("Dato: %b  Dispositivo: %b", this.payload,this.id)
+	endfunction
 
 endclass
