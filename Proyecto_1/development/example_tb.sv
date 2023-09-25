561dda5fa86816ef93893e52c6d5545b7adbfd01
javierespinoza09 <99993066+javierespinoza09@users.noreply.github.com>
2023-09-24 21:40:07 -0600

    Prueba driver (#4)

--- Proyecto_1/development/example_tb.sv
+++ Proyecto_1/development/example_tb.sv
@@ -1,23 +1,42 @@
+`timescale 1ns/1ps
 `include "Library.sv"
 `include "bus_if.sv"
-
+`include "example_driver.sv"
 module example_tb;
 reg clk;
 parameter drvrs = 4; 
 parameter pckg_sz = 16;
+virtual bus_if #(.drvrs(drvrs), .pckg_sz(pckg_sz)) bus_if;
 
-
-
-bs_gnrtr_n_rbtr #( parameter bits = 1,
-parameter drvrs = 4,
-parameter pckg_sz = 16,
-parameter broadcast = {8{1'b1}}) (
-input clk,
-input reset,
-input pndng[bits-1:0][drvrs-1:0],
-output push[bits-1:0][drvrs-1:0],
-output pop[bits-1:0][drvrs-1:0],
-input [pckg_sz-1:0] D_pop[bits-1:0][drvrs-1:0],
-output [pckg_sz-1:0] D_push[bits-1:0][drvrs-1:0]
+bs_gnrtr_n_rbtr #(
+.drvrs(drvrs),
+.pckg_sz(pckg_sz)
+) (
+.clk(bus_if.clk),
+.reset(bus_if.rst),
+.pndng(bus_if.pndng),
+.push(bus_if.push),
+.pop(bus_if.pop),
+.D_pop(bus_if.D_pop),
+.D_push(bus_if.D_push)
 )
+always #5 clk = ~clk;
 
+example_driver driver [drvrs];
+
+initial begin
+    for (int i = 0; i < drvrs; i++) begin
+        driver[i] = new(i);
+        driver[i].randomize();
+        driver[i].v_if = bus_if;
+    end
+    
+    for (int i = 0; i < drvrs; i++) begin
+        fork begin
+            driver[i].run();
+        end
+        join
+    end 
+end
+
+endmodule
