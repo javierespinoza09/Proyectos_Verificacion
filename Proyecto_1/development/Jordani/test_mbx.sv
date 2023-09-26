84b8e917f21cf8d197491b42f49a9cab3cf5fe74
Jordani Mejia <jordanymq99@gmail.com>
2023-09-25 23:27:29 -0600

    Todas las clases para los mailbox definidas

--- Proyecto_1/development/Jordani/test_mbx.sv
+++ Proyecto_1/development/Jordani/test_mbx.sv
@@ -1,4 +1,60 @@
-//Clases de los Mailbox
+///////////////////////////////////
+///////Clases de los Mailbox///////
+///////////////////////////////////
+
+//clases aún no usadas//
+class tst_gen;
+  int caso;
+  function new ();
+  endfunction;
+endclass
+
+
+class tst_chk_sb;
+  int reporte;
+  int retardo;
+  int miss_hit;
+  function new ();
+  endfunction;
+endclass
+
+class gen_ag;
+  int cant_datos;
+  int modo;
+  int id_rand;
+  int id;
+  int source_rand;
+  int source;
+  function new ();
+  endfunction;
+endclass
+
+class mon_chk_sb;
+  int id;
+  int payload;
+  int receiver;
+  int tiempo;
+  function new ();
+  endfunction;
+endclass
+
+//clases en uso//
+
+class ag_dr #(parameter packagesize = 16);
+  bit [packagesize:0] dato;
+  int tiempo;
+  function new (bit [packagesize:0] dato, int tiempo);
+    this.dato = dato;
+    this.tiempo = tiempo;
+  endfunction;
+  
+  function void print(string tag = "");
+    $display("[%g] %s Tiempo=%g dato=%b",$time,tag,this.tiempo,this.dato);
+  endfunction 
+  
+endclass
+
+
 class ag_chk_sb #(parameter packagesize = 16);
   bit [packagesize-9:0] payload ;
   bit [7:0] id ;
@@ -21,7 +77,7 @@ endclass
 
 
 
-
 ////Mailboxes//////
 typedef mailbox #(ag_chk_sb) ag_chk_sb_mbx ;
+typedef mailbox #(ag_dr) ag_dr_mbx;
 /////
