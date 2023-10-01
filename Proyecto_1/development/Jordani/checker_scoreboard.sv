a05e7428ed23b8a8a465cebed2ac95cf8475c4ba
Jordani Mejia <jordanymq99@gmail.com>
2023-10-01 13:32:10 -0600

    chk_sb ya accede al mbx y muestra que datos recibió

--- Proyecto_1/development/Jordani/checker_scoreboard.sv
+++ Proyecto_1/development/Jordani/checker_scoreboard.sv
@@ -7,6 +7,11 @@ class checker_scoreboard;
     
   endfunction 
   
-  
+  task run();
+    forever begin
+    this.ag_chk_sb_mbx.get(this.ag_chk_sb_transaction);
+      $display("Chk recibió el dato %b  con id %b se envió en el tiempo [%g]",this.ag_chk_sb_transaction.payload,this.ag_chk_sb_transaction.id, this.ag_chk_sb_transaction.payload, this.ag_chk_sb_transaction.transaction_time);
+    end 
+  endtask
   
 endclass
