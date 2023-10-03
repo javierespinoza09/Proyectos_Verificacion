e3b13629971eee8b9b4cb3e9617022ddfe53efca
Jordani Mejia <jordanymq99@gmail.com>
2023-10-02 19:08:46 -0600

    casos de ID

--- Proyecto_1/development/Jordani/agente.sv
+++ Proyecto_1/development/Jordani/agente.sv
@@ -1,19 +1,22 @@
 //`include "Clases_mailbox.sv"
 class Agente #(parameter drvrs = 4, parameter pckg_sz = 16);
   
-    	ag_dr_mbx ag_dr_mbx_array [drvrs];
-  		gen_ag_mbx gen_ag_mbx;
-  		ag_chk_sb_mbx ag_chk_sb_mbx;
-    //gen_ag gen_ag;
+	//Mailboxes
+    ag_dr_mbx ag_dr_mbx_array [drvrs];
+  	gen_ag_mbx gen_ag_mbx;
+  	ag_chk_sb_mbx ag_chk_sb_mbx;
+    //Transacciones
 	gen_ag gen_ag_transaction;
   	ag_chk_sb ag_chk_sb_transaction;
   
-	ag_dr #(.pckg_sz(pckg_sz)) ag_dr_transaction;
-    	int num_transacciones;
-    	int delay;
-    	int source;
+	ag_dr #(.pckg_sz(pckg_sz),.drvrs(drvrs)) ag_dr_transaction;
 
+	//Atributos principales
+    int num_transacciones;
+    int delay;
+    int source;
 
+	
   
     function new();
 	this.ag_dr_transaction = new();
@@ -33,19 +36,51 @@ class Agente #(parameter drvrs = 4, parameter pckg_sz = 16);
       	//get desde el generador al agente//
         this.gen_ag_mbx.get(this.gen_ag_transaction);
         this.num_transacciones = this.gen_ag_transaction.cant_datos;
-          $display("Transaccion gen_ag recibida cant %d en el tiempo %d", this.num_transacciones,$time);
-    
-      	
-      
+        $display("Transaccion gen_ag recibida cant %d en el tiempo %d", this.num_transacciones,$time);
+		
         for (int i = 0; i < this.num_transacciones; i++) begin
-		this.ag_dr_transaction = new();
-            	this.ag_dr_transaction.randomize();
-          		this.ag_dr_transaction.tiempo = $time;
-	        //$display("Mensaje enviado a %d Antes",this.ag_dr_transaction.source);
-            	this.ag_dr_mbx_array[this.ag_dr_transaction.source].put(this.ag_dr_transaction);
-          	//comunicación con el checker/scoreboard
-          		this.ag_chk_sb_transaction = new(this.ag_dr_transaction.dato, this.ag_dr_transaction.id, this.ag_dr_transaction.tiempo, this.ag_dr_transaction.source);
-          		this.ag_chk_sb_mbx.put(ag_chk_sb_transaction);
+			this.ag_dr_transaction = new();
+			case (this.gen_ag_transaction.data_modo)
+				max_variabilidad: ag_dr_transaction.data_variablility.constraint_mode(1);
+				max_aleatoriedad: ag_dr_transaction.data_variablility.constraint_mode(0);
+				default: max_aleatoriedad: ag_dr_transaction.data_variablility.constraint_mode(0);
+			endcase
+
+			case (this.gen_ag_transaction.id_modo)
+				self_id: begin 
+					ag_dr_transaction.self_addrs.constraint_mode(0);
+					ag_dr_transaction.valid_addrs.constraint_mode(1);
+				end
+				any_id: begin 
+					ag_dr_transaction.self_addrs.constraint_mode(0);
+					ag_dr_transaction.valid_addrs.constraint_mode(0);
+				end
+				invalid_id: begin 
+					ag_dr_transaction.self_addrs.constraint_mode(1);
+					ag_dr_transaction.valid_addrs.constraint_mode(0);
+				end
+				normal_id: begin 
+					ag_dr_transaction.self_addrs.constraint_mode(1);
+					ag_dr_transaction.valid_addrs.constraint_mode(1);
+				end
+				default: begin 
+					ag_dr_transaction.self_addrs.constraint_mode(1);
+					ag_dr_transaction.valid_addrs.constraint_mode(1);
+				end
+			endcase
+			///Se randomizan los parámetros según las restricciones
+			this.ag_dr_transaction.randomize();
+			///Se evalúa si el paquete requere que el ID, Source o ambos en cada paquete sea previamente determinado
+			if(this.gen_ag_transaction.id_rand==1) ag_dr_transaction.id = gen_ag_transaction.id;
+			if(this.gen_ag_transaction.source_rand==1) ag_dr_transaction.source = gen_ag_transaction.source;
+			///Se carga el tiempo de la transacción 
+          	this.ag_dr_transaction.tiempo = $time;
+			///Se envía el paquete al mailbox corresponciente
+            this.ag_dr_mbx_array[this.ag_dr_transaction.source].put(this.ag_dr_transaction);
+
+
+          	this.ag_chk_sb_transaction = new(this.ag_dr_transaction.dato, this.ag_dr_transaction.id, this.ag_dr_transaction.tiempo, this.ag_dr_transaction.source);
+          	this.ag_chk_sb_mbx.put(ag_chk_sb_transaction);
           
           
 	        $display("Mensaje enviado a %d con id: %d y payload: %d",this.ag_dr_transaction.source,this.ag_dr_transaction.id, this.ag_dr_transaction.dato);
