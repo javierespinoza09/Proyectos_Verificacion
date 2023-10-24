06bf0417af05ddfd0b5b4957e5316f627d30ee52
javierespinoza09 <javierespriv44@gmail.com>
2023-10-23 20:14:51 -0600

    CodigoGenerador

--- Proyecto_2/src/listener.sv
+++ Proyecto_2/src/listener.sv
@@ -14,11 +14,11 @@ class listener;
 			end 
 			end
 			begin
-                                forever begin
-                                        @(posedge router_tb.DUT._rw_[1]._clm_[2].rtr._nu_[3].rtr_ntrfs_.pop);
-                                        $display("POP t:%d r:%d c:%d", 3, 1, 2,$time);
-                        end
-                        end
+                forever begin
+                    @(posedge router_tb.DUT._rw_[1]._clm_[2].rtr._nu_[3].rtr_ntrfs_.pop);
+                    $display("POP t:%d r:%d c:%d", 3, 1, 2,$time);
+                end
+            end
 
 
 		join_none
