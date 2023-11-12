2fd39090b3d06f1bbe3aac1cdb03af60e7fdfd13
javierespinoza09 <99993066+javierespinoza09@users.noreply.github.com>
2023-11-11 19:57:11 -0600

    Inicio fino (#36)
    * Vamoh con todo
    
    * Vamoh con todo

--- Proyecto_3/src/run_vcs.sh
+++ Proyecto_3/src/run_vcs.sh
@@ -1,6 +1,6 @@
 source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools.sh;
-rm -rfv `ls | grep -v -E "*\.sv|*\.sh|*\.py|*\.md"`;
+rm -rfv `ls | grep -v -E "*\.sv|*\.sh|*\.py|*\.md|*\.svh"`;
 echo Cual es el nombre del test bench
 read test
-vcs -Mupdate $test -o salida  -full64 -sverilog  -kdb -lca -debug_acc+all -debug_region+cell+encrypt -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert
+vcs -Mupdate $test -o salida  -full64 -sverilog  -kdb -lca -debug_acc+all -debug_region+cell+encrypt -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert -ntb_opts uvm-1.2
 
